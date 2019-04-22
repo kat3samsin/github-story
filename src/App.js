e7b87926ba9945f0fe660a073058c0248149735f
katre <kat3samsin@gmail.com>
2019-04-22 14:16:01 +0800

    removed clutter, added gh-pages dependency

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn more here
+            Start here!
           </a>
         </header>
       </div>
