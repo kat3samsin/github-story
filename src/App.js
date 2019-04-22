0c2fcb56533cd655f1b60272cf0aff2b59e58b2f
katre <kat3samsin@gmail.com>
2019-04-22 12:58:52 +0800

    add boilerplate code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/kat3samsin/supersimple-react"
             target="_blank"
             rel="noopener noreferrer"
           >
