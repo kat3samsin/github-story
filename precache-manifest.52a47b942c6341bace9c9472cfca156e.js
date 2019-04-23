ced6f17841f0b60d561eb70b52383953177b8750
System Administrator <root@gummybear.local>
2019-04-23 16:33:53 +0800

    Updates

---
+++ precache-manifest.52a47b942c6341bace9c9472cfca156e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd684ffcf8ea6262657b",
+    "url": "/github-story/static/js/runtime~main.0a21f34a.js"
+  },
+  {
+    "revision": "3b08dc5d72ed3d26ae43",
+    "url": "/github-story/static/js/main.fae1539e.chunk.js"
+  },
+  {
+    "revision": "43a91e9fc9a25d0fb7e1",
+    "url": "/github-story/static/js/2.43e4770d.chunk.js"
+  },
+  {
+    "revision": "3b08dc5d72ed3d26ae43",
+    "url": "/github-story/static/css/main.afe8c73a.chunk.css"
+  },
+  {
+    "revision": "7a5efa9f6bb66259e195507c3e7c0781",
+    "url": "/github-story/index.html"
+  }
+];
